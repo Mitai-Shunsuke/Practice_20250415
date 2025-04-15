b044e8b0a88865056abe680901f1c725d2acd812
Mitai-Shunsuke <s.mitai@technical-agent.jp>
2025-04-15 16:22:48 +0900

    ログイン画面作成2

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace StockControlSystem
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnLogin.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnLogin.Location = new System.Drawing.Point(205, 272);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(358, 53);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "ログイン";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(199, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 49);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ログイン画面";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(202, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "社員名";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(202, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "パスワード";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtName.Location = new System.Drawing.Point(205, 121);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(358, 48);
+            this.txtName.TabIndex = 4;
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtPass.Location = new System.Drawing.Point(205, 203);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '●';
+            this.txtPass.Size = new System.Drawing.Size(358, 48);
+            this.txtPass.TabIndex = 5;
+            this.txtPass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "frmLogin";
+            this.Text = "ログイン";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
---
+++ frmLogin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockControlSystem
+{
+    public partial class frmLogin : Form
+    {
+        //■プロパティ
+        //コンストラクタ
+        pgSelectSQL bat = new pgSelectSQL();
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        //■ログインボタン
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //入力チェック
+            if(!CheckInput())
+            {
+                return;
+            }
+
+            //SQL取得
+            string Query = CreateSQL_Staff();
+
+            //パラメータ追加
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@Name",txtName.Text));
+            parameters.Add(new SqlParameter("@Pass",txtPass.Text));
+
+            //実行
+            if(bat.SelectSQL(Query, parameters))
+            {
+                //メインメニュー画面遷移
+                frmMainMenu frmMainMenu = new frmMainMenu();
+                frmMainMenu.ShowDialog();
+            }
+        }
+
+        //■入力チェック
+        private bool CheckInput()
+        {
+            //社員名
+            if(txtName.Text == "")
+            {
+                MessageBox.Show("社員名を入力をしてください。");
+                return false;
+            }
+
+
+            //パスワード
+            if (txtPass.Text == "")
+            {
+                MessageBox.Show("パスワードを入力をしてください。");
+                return false;
+            }
+            if(txtPass.Text.Length != 4 || !int.TryParse(txtPass.Text, out int i))
+            {
+                MessageBox.Show("パスワードは4文字の数字で入力してください。");
+                return false;
+            }
+
+            return true;
+        }
+
+        //■SQL作成
+        private string CreateSQL_Staff()
+        {
+            //SQL作成
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("SELECT *");
+            sb.AppendLine("FROM IM_STAFF");
+            sb.AppendLine("WHERE StaffName LIKE @Name");
+            sb.AppendLine("AND Password LIKE @Pass");
+
+            return sb.ToString();
+
+        }
+    }
+}
