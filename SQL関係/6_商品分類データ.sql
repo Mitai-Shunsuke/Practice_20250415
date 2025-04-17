d8274ba21a549c24c5961b132e531fbe4c8f4848
Mitai-Shunsuke <s.mitai@technical-agent.jp>
2025-04-17 13:17:29 +0900

    DBデータ追加

---
+++ SQL関係/6_商品分類データ.sql
@@ -0,0 +1,11 @@
+INSERT INTO IM_ITEM_CLASS(ItemClassCD,ItemClassName,Stocks)
+VALUES(1,'ボールペン',10)
+
+INSERT INTO IM_ITEM_CLASS(ItemClassCD,ItemClassName,Stocks)
+VALUES(2,'用紙',100)
+
+INSERT INTO IM_ITEM_CLASS(ItemClassCD,ItemClassName,Stocks)
+VALUES(3,'マーカー',50)
+
+INSERT INTO IM_ITEM_CLASS(ItemClassCD,ItemClassName,Stocks)
+VALUES(4,'筆箱',30)
---
+++ SQL関係/7_商品データ.sql
@@ -0,0 +1,32 @@
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(1,1,'ボールペン　赤')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(1,2,'ボールペン　青')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(1,3,'ボールペン　緑')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(2,4,'A3用紙')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(2,5,'A4用紙')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(2,6,'B3用紙')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(2,7,'A4用紙')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(3,8,'マーカー　赤')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(3,9,'マーカー　青')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(3,10,'マーカー　緑')
+
+INSERT INTO IM_ITEM(ItemClassCD,ItemCD,ItemName)
+VALUES(3,11,'マーカー　黄')
